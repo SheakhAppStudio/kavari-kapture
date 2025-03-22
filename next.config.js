76e3918032aaffc50b2ebb60a84aa86671b3f01b
SheakhAppStudio <sheakhappstudio@gmail.com>
2025-03-22 01:23:37 +0000

    Add next.config.js for GitHub Pages

--- next.config.js
+++ next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+  assetPrefix: './',
+  basePath: '',
   images: {
     unoptimized: true,
   },
+  trailingSlash: true,
 }
 
 module.exports = nextConfig 
